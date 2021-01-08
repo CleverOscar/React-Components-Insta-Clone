fcfc164cfb105aa036f1051d65125a3299f421de
Oscar Pacheco <Oscarpa88@yahoo.com>
2021-01-07 21:28:24 -0600

    Updated Nav

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
-/* 
-  Start here and work your way down the nested components.
-  Not all files in the project need code added.
-  Look at each file to see what props need to be passed!
-*/
-
-// Import the state hook
-import React from 'react';
-// Import the Posts (plural!) and SearchBar components, since they are used inside App component
-// Import the dummyData
+import React, {useState} from 'react';
+import Posts from './components/Posts/Posts.js';
+import SearchBar from './components/SearchBar/SearchBar.js';
+import dummyData from './dummy-data'
 import './App.css';
 
 const App = () => {
-  // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
-  // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
+  const [posts, setPosts] = useState(dummyData);
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
 
   const likePost = postId => {
@@ -21,7 +14,7 @@ const App = () => {
 
       The state of the app lives at the top of the React tree, but it wouldn't be fair for nested components not to be able to change state!
       This function is passed down to nested components through props, allowing them to increase the number of likes of a given post.
-
+      
       Invoke `setPosts` and pass as the new state the invocation of `posts.map`.
       The callback passed into `map` performs the following logic:
         - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
@@ -31,8 +24,8 @@ const App = () => {
 
   return (
     <div className='App'>
-      {/* Add SearchBar and Posts here to render them */}
-      {/* Check the implementation of each component, to see what props they require, if any! */}
+      <SearchBar />
+      <Posts posts={posts} />
     </div>
   );
 };
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -3,12 +3,14 @@ import Post from './Post';
 import './Posts.css';
 
 const Posts = (props) => {
-  // 🔥 Make sure the parent of Posts is passing the right props!
   const { likePost, posts } = props;
-
+  
   return (
     <div className='posts-container-wrapper'>
-      {/* Map through the posts array returning a Post component at each iteration */}
+      {/* Map through the posts array returning a Post component at each iteration */
+      posts.map(post => 
+        <div>{post.username}</div>)
+      }
       {/* Check the implementation of Post to see what props it requires! */}
     </div>
   );
