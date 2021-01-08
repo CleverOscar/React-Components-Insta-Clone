9a5a42dbca58edf24ef4f2858925826dffb89c8a
Oscar Pacheco <Oscarpa88@yahoo.com>
2021-01-08 00:44:45 -0600

    Updated props

--- src/components/Posts/LikeSection.js
+++ src/components/Posts/LikeSection.js
@@ -1,4 +1,3 @@
-// Use a piece of data coming in through props to display the correct number of likes.
 // You will also add an onClick handler that utilizes `likePost` to increase the count of likes.
 // (As a stretch goal, you might want to prevent your user from "liking" the same post more than once.)
 import React from 'react';
@@ -8,7 +7,7 @@ import { faComment, faHeart } from '@fortawesome/free-regular-svg-icons';
 const LikeSection = props => {
   // 🔥 Make sure the parent of LikeSection is passing the right props!
   const { likePost, numberOfLikes } = props;
-  // console.log(numberOfLikes)
+
   return (
     <div>
       <div
@@ -22,7 +21,7 @@ const LikeSection = props => {
           <FontAwesomeIcon icon={faComment} />
         </div>
       </div>
-      <p className='like-number'>{numberOfLikes}</p>
+      <p className='like-number'>{props.post.likes}</p>
     </div>
   );
 };
